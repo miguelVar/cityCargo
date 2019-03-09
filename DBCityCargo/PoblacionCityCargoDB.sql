2e99f9b4982c00a67b0391d6005572fb531c4102
juan-caceres <juan.caceres@ibm.com>
2019-03-09 12:53:44 -0500

    Update PoblacionCityCargoDB.sql

--- DBCityCargo/PoblacionCityCargoDB.sql
+++ DBCityCargo/PoblacionCityCargoDB.sql
@@ -1,5 +1,10 @@
 -- Insersiones en la tabla Rol
 INSERT INTO Rol VALUES(1,'Administrador','Administrador');
 
+-- Insersiones en la tabla Estado
+INSERT INTO Estado VALUES(1,'Sin Iniciar');
+INSERT INTO Estado VALUES(2,'En Proceso');
+INSERT INTO Estado VALUES(3,'Finalizado');
+
 -- Insersiones en la tabla TipoVehiculo
 INSERT INTO TipoVehiculo(nombreTipo,descripcionTipo) VALUES('Furgon','Vehículo de carga ligera');
