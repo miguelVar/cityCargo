b607e9323dff5fb9f28048b74317ae50eaf159a9
juan-caceres <juan.caceres@ibm.com>
2019-03-09 14:07:49 -0500

    CRUD orden

--- server/controller/orden.controller.js
+++ server/controller/orden.controller.js
@@ -39,7 +39,53 @@ ordenCtrl.createOrden = (req, res) => {
             else res.json({ data: 'Se creo la orden de manera satisfactoria!!!' })
 
             conn.close(function () {
-                console.log('done Listar Clientes');
+                console.log('done Listar Orden');
+            });
+        });
+    });
+
+}
+
+//Método para actualizar una orden
+ordenCtrl.updateOrden = (req, res) => {
+
+    let idOrden = parseInt(req.params.idOrden);
+    let numeroOrden = req.body.NUMEROORDEN;
+    let descripcionOrden = req.body.NUMEROORDEN;
+
+    var query = `UPDATE Orden SET numeroOrden = '${numeroOrden}', descripcionOrden = '${descripcionOrden}' WHERE idOrden = '${idOrden}'`;
+
+    ibmdb.open(connStr, function (err, conn) {
+        if (err) return console.log(err);
+
+        conn.query(query, function (err, data) {
+            if (err) res.json({ error: err })
+            else res.json({ data: 'Se actualizo la orden de manera satisfactoria!!!' })
+
+            conn.close(function () {
+                console.log('done Actualiza Orden');
+            });
+        });
+    });
+
+}
+
+// Método para eliminar orden
+ordenCtrl.deleteOrden = (req, res) => {
+
+    let idOrden = parseInt(req.params.idOrden);
+
+    var query = `DELETE FROM Orden WHERE idOrden = '${idOrden}'`;
+
+    ibmdb.open(connStr, function (err, conn) {
+        if (err) return console.log(err);
+
+        conn.query(query, function (err, data) {
+            if (err) res.json({ error: err })
+            else res.json({ data: 'Se elimino la orden de manera satisfactoria!!!' })
+
+            conn.close(function () {
+                console.log('done elimino Orden');
             });
         });
     });
--- server/routes/orden.routes.js
+++ server/routes/orden.routes.js
@@ -6,5 +6,9 @@ const ordenCtrl = require('../controller/orden.controller');
 router.get('/', ordenCtrl.getOrden);
 // Crear orden
 router.post('/', ordenCtrl.createOrden);
+// Actualiza orden
+router.put('/:idOrden', ordenCtrl.updateOrden);
+//Elimina orden
+router.delete('/:idOrden', ordenCtrl.deleteOrden);
 
 module.exports = router;
