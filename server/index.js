2404ebf9c37b05a30b5fbdc9ff076f0409e92a81
juan-caceres <juan.caceres@ibm.com>
2019-03-09 11:29:24 -0500

    modificaciones en el server.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
 const express=require('express');
 const cors=require('cors');
 const app=express();
-const ibmdb=require('../server/config/database');
 
 //settings
 app.set('port',process.env.PORT || 3000);
 
 // //middleware
-// app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors({origin: 'http://localhost:4200'}));
 
 //routes
-// app.use('/api/clients',require('./routes/client.routes'));
+app.use('/api/orden',require('./routes/orden.routes'));
 
 
 //starting
 app.listen(app.get('port'),()=>{
-    console.log('corriendo');
+    console.log('Servidor corriendo en el puerto', app.get('port'));
 })
