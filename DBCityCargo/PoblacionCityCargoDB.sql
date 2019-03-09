4859ec8a658865d00b70a2ee9a8f5a5dbd77c609
juan-caceres <juan.caceres@ibm.com>
2019-03-09 08:33:25 -0500

    Base de datos CityCargo S.A.S

---
+++ DBCityCargo/PoblacionCityCargoDB.sql
@@ -0,0 +1,5 @@
+-- Insersiones en la tabla Rol
+INSERT INTO Rol VALUES(1,'Administrador','Administrador');
+
+-- Insersiones en la tabla TipoVehiculo
+INSERT INTO TipoVehiculo(nombreTipo,descripcionTipo) VALUES('Furgon','Vehículo de carga ligera');
