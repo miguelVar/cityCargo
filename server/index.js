603249efdfb3169becff9348e014d33e0864288e
juan-caceres <juan.caceres@ibm.com>
2019-03-09 17:21:09 -0500

    CRUD Tipo Vehiculo

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(cors({origin: 'http://localhost:4200'}));
 app.use('/api/orden', require('./routes/orden.routes'));
 app.use('/api/vehiculo', require('./routes/vehiculo.routes'));
 app.use('/api/conductor', require('./routes/conductor.routes'));
+app.use('/api/tipovehiculo', require('./routes/tipoVehiculo.routes'));
 
 //starting
 app.listen(app.get('port'),()=>{
