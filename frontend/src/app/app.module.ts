b8c3078ec414ad91568224fd5601f4a0ded67d92
juan-caceres <juan.caceres@ibm.com>
2019-03-23 09:44:21 -0500

    agregados form modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { APP_ROUTING } from './app.routes';
+import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTING
+    APP_ROUTING,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
