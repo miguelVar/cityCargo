96841988ad91aaee2635e3afdca42b4b4650419b
juan-caceres <juan.caceres@ibm.com>
2019-03-23 14:32:14 -0500

    detalles en navbar

--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router:Router) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.loginService.logout()
+      .subscribe((data) => {
+        localStorage.setItem('usuario', data['token']);
+        localStorage.setItem('rol', null);
+        console.log('Saliendo');
+        this.router.navigate(['login'])
+      });
   }
 
 }
