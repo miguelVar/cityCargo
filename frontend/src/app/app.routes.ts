d12c465cd6f7fc40e7ead9d4e02efc0323cf861a
juan-caceres <juan.caceres@ibm.com>
2019-03-23 13:09:30 -0500

    agregado path de home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule, CanActivate } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'login' }
+    { path: '**', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'home', component: HomeComponent }
 ]
 
 export const APP_ROUTING = RouterModule.forRoot(routes);
