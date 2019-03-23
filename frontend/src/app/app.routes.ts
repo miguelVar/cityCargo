b15bd5cf798b5744a66ba9ef890ad201dd146c03
juan-caceres <juan.caceres@ibm.com>
2019-03-23 13:49:48 -0500

    funciona ruta home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes, RouterModule, CanActivate } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { GuardService } from './services/guard.service'
 
 const routes: Routes = [
+    { path: 'home', component: HomeComponent, canActivate: [GuardService] },
     { path: 'login', component: LoginComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'login' },
-    { path: 'home', component: HomeComponent }
+    { path: '**', pathMatch: 'full', redirectTo: 'login' }
 ]
 
 export const APP_ROUTING = RouterModule.forRoot(routes);
