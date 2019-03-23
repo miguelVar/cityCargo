fb3fea019c8520d157b54dd6e1cc08ade273ca53
JUANDA-07 <juan.caceres02@uptc.edu.co>
2019-03-23 12:43:11 -0500

    Update PoblacionCityCargoDB.sql

--- DBCityCargo/PoblacionCityCargoDB.sql
+++ DBCityCargo/PoblacionCityCargoDB.sql
@@ -1,6 +1,9 @@
 -- Insersiones en la tabla Rol
 INSERT INTO Rol VALUES(1,'Administrador','Administrador');
 
+-- Insersion de usuario
+INSERT INTO Usuario (nombreUsuario,correo,password) VALUES('Admin','Admin@citycargo.com','12345');
+
 -- Insersiones en la tabla Estado
 INSERT INTO Estado VALUES(1,'Sin Iniciar');
 INSERT INTO Estado VALUES(2,'En Proceso');
